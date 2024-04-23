dc647d83489e98de4c80e3fd12e2a28fec487011
Brian Cilenti <briancilenti@gmail.com>
2024-04-23 15:55:14 -0400

    venue contact take two

---
+++ prisma/migrations/20240423195502_venue_contact/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "VenueContact" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "phone" TEXT,
+    "email" TEXT,
+    "name" TEXT NOT NULL,
+    "isPrimary" BOOLEAN,
+    "status" TEXT,
+    "venueId" TEXT NOT NULL,
+    CONSTRAINT "VenueContact_venueId_fkey" FOREIGN KEY ("venueId") REFERENCES "Venue" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VenueContact_venueId_key" ON "VenueContact"("venueId");
+
+-- CreateIndex
+CREATE INDEX "VenueContact_venueId_idx" ON "VenueContact"("venueId");
